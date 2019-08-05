bbf31484b1691ea05d74c368dd4d9f745fe4f695
luther <maseratipp@gmail.com>
2019-08-05 23:14:56 +0800

    init service

---
+++ README.md
@@ -0,0 +1,27 @@
+# 文档说明
+
+## eureka
+- 服务治理
+- 替代： zookeeper、redis、springNocas
+## ribbon
+- 负载均衡
+## hystrix
+- 服务容错-> 服务降级
+- 替代： sentinel（阿里）
+## feign
+- 服务调用
+## zuul
+- 服务网关
+## config
+- 分布式配置
+## bus
+- 消息总线
+## stream
+- 消息驱动
+## sleuth
+- 服务跟踪
+
+## dubbo
+- RPC框架关联SpringCloudAlibaba
+## seata
+- 分布式事务
---
+++ stream/src/test/java/com/luther/cloud/stream/StreamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luther.cloud.stream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StreamApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
